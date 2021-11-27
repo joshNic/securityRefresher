1ea9107f11e0aecdedf5c39c878586a68377285e
joshNic <256jomu@gmail.com>
2021-11-27 16:52:58 -0600

    setup spring security

---
+++ src/main/java/com/example/securityrefresher/SecurityRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityrefresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRefresherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityRefresherApplication.class, args);
+    }
+
+}
--- src/main/java/com/example/securityrefresher/controllers/HomeController.java
+++ src/main/java/com/example/securityrefresher/controllers/HomeController.java
@@ -1,2 +1,12 @@
-package com.example.securityrefresher.controllers;public class HomeController {
+package com.example.securityrefresher.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String welcome(){
+        return ("<h1>Welcome</h1>");
+    }
 }
