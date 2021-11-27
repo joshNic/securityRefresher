bd58bdec2988ef78ac622f1d3f21a9b74cd8a624
joshNic <256jomu@gmail.com>
2021-11-27 17:16:15 -0600

    add authorization

--- src/main/java/com/example/securityrefresher/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/securityrefresher/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.securityrefresher.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,11 +15,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("test")
                 .password("test")
-                .roles("TEST");
+                .roles("TEST")
+                .and()
+                .withUser("test2")
+                .password("test2")
+                .roles("ADMIN");
     }
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                .antMatchers("/**")
+                .hasRole("ADMIN")
+                .and()
+                .formLogin();
     }
 
 }
