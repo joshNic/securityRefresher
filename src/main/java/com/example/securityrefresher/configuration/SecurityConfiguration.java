569210bf4893fabc4028894c96ef95a9890b4f0e
joshNic <256jomu@gmail.com>
2021-11-27 17:29:55 -0600

    add more routes for more authorization

--- src/main/java/com/example/securityrefresher/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/securityrefresher/configuration/SecurityConfiguration.java
@@ -3,11 +3,13 @@ package com.example.securityrefresher.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception{
@@ -30,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("TEST")
+                .antMatchers("/user").hasAnyRole("TEST","ADMIN")
                 .antMatchers("/").permitAll()
                 .and()
                 .formLogin();
