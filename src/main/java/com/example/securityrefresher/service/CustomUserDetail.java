1c03da6091bb37debe295d87ae7f839b679661f5
joshNic <256jomu@gmail.com>
2021-11-28 08:55:14 -0600

    configure spring security to depend on userdetailservice

---
+++ src/main/java/com/example/securityrefresher/service/CustomUserDetail.java
@@ -0,0 +1,56 @@
+package com.example.securityrefresher.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class CustomUserDetail implements UserDetails {
+
+    private String userName;
+
+    public CustomUserDetail(String userName){
+        this.userName=userName;
+    }
+
+    public CustomUserDetail(){
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "pass";
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
