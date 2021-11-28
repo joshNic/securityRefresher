64bface7b6df2659920462b9f6b99a0245564c54
joshNic <256jomu@gmail.com>
2021-11-28 09:30:44 -0600

    add jpa and wire mysql db to retrieve actual users

--- src/main/java/com/example/securityrefresher/SecurityRefresherApplication.java
+++ src/main/java/com/example/securityrefresher/SecurityRefresherApplication.java
@@ -1,9 +1,12 @@
 package com.example.securityrefresher;
 
+import com.example.securityrefresher.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SecurityRefresherApplication {
 
     public static void main(String[] args) {
