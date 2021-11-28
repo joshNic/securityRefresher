d2ee5b3ede77a7f1298fd9890042b3169542dc32
joshNic <256jomu@gmail.com>
2021-11-28 16:58:47 -0600

    add authentication manager bean

--- src/main/java/com/example/securityrefresher/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/securityrefresher/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.securityrefresher.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -56,6 +57,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeRequests()
